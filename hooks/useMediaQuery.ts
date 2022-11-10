d0c2eada073f709757f30080405276e9106d849f
Jonathan Cazzaro <jonathan.cazzaro@gmail.com>
2022-11-10 12:02:16 +0100

    added function overloads to hook

--- hooks/useMediaQuery.ts
+++ hooks/useMediaQuery.ts
@@ -1,10 +1,19 @@
 import { useState, useEffect } from "react";
 
-export const useMediaQuery = (customQuery?: string) => {
+interface DefaultQueries {
+  isWidthLarger: (value: number) => boolean;
+  isWidthSmaller: (value: number) => boolean;
+  isHeightLarger: (value: number) => boolean;
+  isHeightSmaller: (value: number) => boolean;
+}
+
+export function useMediaQuery(): DefaultQueries;
+export function useMediaQuery(customQuery: string): boolean;
+export function useMediaQuery(customQuery?: string) {
   const setQuery = (queryConfig: string): boolean => {
     const [verified, setVerified] = useState(false);
     const query = matchMedia(queryConfig);
-    
+
     const handleChange = (event: MediaQueryListEvent) => {
       if (event.matches !== verified) setVerified(event.matches);
       query.removeEventListener("change", handleChange);
@@ -23,4 +32,4 @@ export const useMediaQuery = (customQuery?: string) => {
   const isHeightLarger = (value: number) => setQuery(`(min-height: ${value}px)`);
   const isHeightSmaller = (value: number) => setQuery(`(max-height: ${value}px)`);
   return { isWidthSmaller, isWidthLarger, isHeightSmaller, isHeightLarger };
-};
+}
